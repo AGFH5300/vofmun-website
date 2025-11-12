fc73e2f44cb3219405a17b5cd9905a168fa7b031
AGFH5300 <dxb.avg@gmail.com>
2025-11-12 22:20:36 +0400

    Delete docs directory

--- docs/sql/add_delegate_referral_codes.sql
+++
@@ -1,8 +0,0 @@
--- Adds a JSONB column for storing multiple referral codes submitted by delegates.
-ALTER TABLE public.users
-  ADD COLUMN IF NOT EXISTS delegate_referral_codes jsonb;
-
--- Ensure existing delegate rows have an empty array to simplify querying.
-UPDATE public.users
-SET delegate_referral_codes = '[]'::jsonb
-WHERE role = 'delegate' AND delegate_referral_codes IS NULL;
