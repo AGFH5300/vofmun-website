f29c922af887d3fd71e3203e70d1dcc4baf1b2ca
AGFH5300 <dxb.avg@gmail.com>
2026-02-14 15:52:51 +0400

    Restore Tala referral code and adjust carousel role punctuation

--- components/founders-modal.tsx
+++ components/founders-modal.tsx
@@ -7,18 +7,13 @@ import Link from "next/link"
 
 const founders = [
   {
-    name: "Tala Swaidan",
-    role: "Founder & Secretary-General",
-    image: "/founders/TalaSwaidan_SG.png",
-  },
-  {
     name: "Vihaan Shukla",
     role: "Co-Founder & Director General",
     image: "/founders/VihaanShukla_DG.png",
   },
   {
     name: "Ansh Gupta",
-    role: "Head of Technology",
+    role: "Co-Founder & Head of Technology",
     image: "/founders/AnshGupta_Tech.png",
   },
   {
